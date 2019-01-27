818ee58233ed11ba9b897cde142928731f0ee12e
Samantha Wong <smiwong920@gmail.com>
2019-01-27 20:12:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ katzDeliLine = []
 
 function takeANumber(lineNumber, name) {
   for ( var i = 0; i < lineNumber.length; i++) { 
-   console.log("Welcome, " + name[i] + ". You are number " + (lineNumber[i]+1) + " in line.")
+   push("Welcome, " + name[i] + ". You are number " + (lineNumber[i]+1) + " in line.")
 }
 }
