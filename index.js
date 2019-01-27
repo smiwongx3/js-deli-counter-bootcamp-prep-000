4569bf07e440eeb5c5147b96076d78b89feb336b
Samantha Wong <smiwong920@gmail.com>
2019-01-27 21:12:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
   console.log("Currently serving " + katzDeliLine[0] + ".")
-  katzDeliLine.shift()
 } else {
   return ("There is nobody waiting to be served!")
   }
