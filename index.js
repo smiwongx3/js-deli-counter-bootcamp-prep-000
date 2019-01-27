d356e8d299980bfd41fe4f2ae7d82d3c0b68eba0
Samantha Wong <smiwong920@gmail.com>
2019-01-27 21:29:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,10 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  
+  if (katzDeliLine.length > 0) {
+    return("The line is currently:")
+} else {
+  return("The line is currently empty.")
+}
+  }
 }
